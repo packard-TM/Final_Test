6f97e83206b53562672bd20ac4fa1e7b534f98bf
packard_TM <a174126725@gmail.com>
2024-02-28 13:57:04 +0300

    сщздали git купозиторий

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] arrayStart = { "ехали", "медведи", "на", "велосипеде", "а", "за", "ними", "раки", "на", "хромой", "собаке", "вот", "и", "сказочке", "esc", "кто", "не", "понял", "F1"};
+string[] arrayResult = new string[arrayStart.Length];
+int count = 0;
+
+for (int i = 0; i < arrayStart.Length; i++)
+{
+    if (arrayStart[i].Length < 4)
+    {
+        arrayResult[count] = arrayStart[i];
+        count++;
+    }
+}
+
+string[] arrayItog = new string[count];
+
+for (int i = 0; i < count; i++)
+{
+    arrayItog[i] = arrayResult[i];
+}
+
+Console.WriteLine(string.Join(", ", arrayItog));
